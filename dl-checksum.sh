b33a598220446d8dec6598b78e575f6bf91a3f74
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-07-14 13:52:06 -0400

    gradle:7.1.1

---
+++ dl-checksum.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://services.gradle.org/distributions
+
+# https://services.gradle.org/distributions/gradle-6.6-bin.zip.sha256
+# https://services.gradle.org/distributions/gradle-7.1.1-bin.zip.sha256
+
+dl_ver()
+{
+    local ver=$1
+    local archive_type=${4:-zip}
+
+    local url=$MIRROR/gradle-${ver}-bin.zip.sha256
+
+    printf "  # %s\n" $url
+    printf "  %s: sha256:%s\n" $ver $(curl -sSL $url)
+}
+
+dl_ver ${1:-7.1.1}
